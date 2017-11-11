82567394ed3030e7f4f8d419260d59dfe778f75d
jiminycricket <jiminy.upon.star@gmail.com>
2017-11-11 14:44:04 +0800

    init probot

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = robot => {
+  robot.on('pull_request_review.submitted', async context => {
+    const repoOwner = context.payload.pull_request.user.login;
+    const reviwer = context.payload.review.user.login;
+    const commentMsg = `unassign @${reviwer}, assign @${repoOwner}`;
+
+    const commentParams = context.issue({ body: commentMsg })
+    const addAssigneesParams = context.issue({ assignees: [repoOwner] })
+    const removeAssigneesParams = context.issue({ body: {
+      assignees: [reviwer] }
+    })
+
+    context.github.issues.addAssigneesToIssue(addAssigneesParams);
+    context.github.issues.removeAssigneesFromIssue(removeAssigneesParams);
+    return context.github.issues.createComment(commentParams);
+  });
+}
