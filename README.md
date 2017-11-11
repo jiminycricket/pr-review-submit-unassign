e2a1adec2c6f14a712a966ff55b8e02b35516548
jiminycricket <jiminy.upon.star@gmail.com>
2017-11-11 23:02:54 +0800

    add config from pr_review_submit_unassign.yml

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # pr-review-submit-unassign
 
 > a GitHub App built with [probot](https://github.com/probot/probot) that 
+> can unasign reviewer and asign pull request owner when pull request review submited.
 
 ## Setup
 
@@ -13,3 +14,15 @@ npm start
 ```
 
 See [docs/deploy.md](docs/deploy.md) if you would like to run your own instance of this app.
+
+## Config
+
+set up ``` .github/pr_review_submit_unassign.yml```
+
+default:
+```
+commentUnassign: 'unassign @{reviwer}',
+commentAssign: 'assign @{repoOwner}',
+unAssignReviwer: true,
+assignPrOwner: true
+``` 
--- index.js
+++ index.js
@@ -1,19 +1,41 @@
+const defaultConfig = require('./lib/defaultConfig');
+
 module.exports = robot => {
   robot.on('pull_request_review.submitted', async context => {
+
     const repoOwner = context.payload.pull_request.user.login;
     const reviwer = context.payload.review.user.login;
-    const commentMsg = `unassign @${reviwer}, assign @${repoOwner}`;
+
+    let commentMsg = ``;
+
     if (repoOwner === reviwer) {
       return;
     }
-    const commentParams = context.issue({ body: commentMsg })
-    const addAssigneesParams = context.issue({ assignees: [repoOwner] })
-    const removeAssigneesParams = context.issue({ body: {
-      assignees: [reviwer] }
-    })
 
-    context.github.issues.addAssigneesToIssue(addAssigneesParams);
-    context.github.issues.removeAssigneesFromIssue(removeAssigneesParams);
-    return context.github.issues.createComment(commentParams);
+    const userConfig = await context.config('pr_review_submit_unassign.yml');
+    const config = Object.assign({}, defaultConfig, userConfig);
+
+    if (config.unAssignReviwer) {
+      const removeAssigneesParams = context.issue({
+        body: {
+          assignees: [reviwer]
+        }
+      });
+      context.github.issues.removeAssigneesFromIssue(removeAssigneesParams);
+      commentMsg += config.commentUnassign.replace('{reviwer}', reviwer);
+    }
+    
+    if (config.assignPrOwner) {
+      const addAssigneesParams = context.issue({ assignees: [repoOwner] });
+      context.github.issues.addAssigneesToIssue(addAssigneesParams);
+      commentMsg += (config.unAssignReviwer ? ', \n' : '') + config.commentAssign.replace('{repoOwner}', repoOwner)
+    }
+
+    if (commentMsg) {
+      const commentParams = context.issue({ body: commentMsg });
+      context.github.issues.createComment(commentParams);
+    }
+
+    return;
   });
 }
